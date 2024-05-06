c4fcb949e1e9a5c01f52624a5daf73773accf9c8
iBearchic <14052001max@gmail.com>
2024-05-07 01:09:43 +0300

    binance data collector was changed to bybit due issues

--- src/config/secrets.py
+++ src/config/secrets.py
@@ -5,7 +5,7 @@ def load_secrets() -> tuple:
     # Загрузка переменных окружения 
     load_dotenv()  
     
-    api_key = os.getenv('BINANCE_API_KEY')
-    api_secret = os.getenv('BINANCE_API_SECRET')
+    api_key = os.getenv('BYBIT_API_KEY')
+    api_secret = os.getenv('BYBIT_API_SECRET')
     return api_key, api_secret
 
